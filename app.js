5c36d897ca71a766bb4bbd6005f2a85b1bd9307c
matthewh900 <matthewh900@gmail.com>
2025-01-27 16:14:44 +0000

    add model and controller for GET: /api/topics endpoint

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require("express")
-const app = express()
-const endpoints = require("./endpoints.json")
-const {getEndpoints} = require("./controllers/controllers")
+const express = require("express");
+const app = express();
+const endpoints = require("./endpoints.json");
+const { getTopics } = require("./controllers/controllers");
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api", (req, res) => {
-    res.status(200).send({endpoints})
-})
+  res.status(200).send({ endpoints });
+});
 
-module.exports = app
+app.get("/api/topics", getTopics);
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+module.exports = app;
