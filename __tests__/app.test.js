22e5e4747eb2036a738265406e0fdc3d17045742
matthewh900 <matthewh900@gmail.com>
2025-01-27 14:09:55 +0000

    add endpoint for GET:/api/ and set up test suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,12 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const app = require("../app")
+const request = require("supertest")
+const db = require("../db/connection")
+const seed = require("../db/seeds/seed")
+const data = require("../db/data/test-data/index")
 
-/* Set up your beforeEach & afterAll functions here */
+beforeEach(() => seed(data))
+afterAll(() => db.end())
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
