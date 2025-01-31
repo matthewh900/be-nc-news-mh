8e440ad2895defa392d28ba7ffcff2af61f4f492
matthewh900 <matthewh900@gmail.com>
2025-01-31 12:09:41 +0000

    remove .only

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -34,7 +34,7 @@ describe("GET /api/topics", () => {
   });
 });
 
-describe.only("GET /api/articles/:article_id", () => {
+describe("GET /api/articles/:article_id", () => {
   test("should respond with status code 200 and article with matching article_id property", () => {
     return request(app)
       .get("/api/articles/1")
